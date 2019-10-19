dc86aee5a71c2b68256ab4bedd4796e11ca9809f
David Ankin <daveankin@gmail.com>
2019-10-19 17:32:44 -0400

    try faster script

--- make_update_links.sh
+++ make_update_links.sh
@@ -6,7 +6,7 @@ links_doc="$repo_dir/snippets/imports/cdn.sublime-snippet"
 old_cdn_css="none"
 old_cdn_jq="none"
 old_cdn_popper="none"
-old_cdn_bootstrap="none"
+old_cdn_bs="none"
 
 function replace () {
   sed -i -e "s\\$1\\$2\\g" $3
@@ -25,28 +25,29 @@ while IFS= read -r line; do
     elif [[ "$old_cdn_popper" == "none" ]]; then
       old_cdn_popper="$line"
       echo "set old_cdn_popper $old_cdn_popper"
-    elif [[ "$old_cdn_bootstrap" == "none" ]]; then
-      old_cdn_bootstrap="$line"
-      echo "set old_cdn_bootstrap $old_cdn_bootstrap"
+    elif [[ "$old_cdn_bs" == "none" ]]; then
+      old_cdn_bs="$line"
+      echo "set old_cdn_bs $old_cdn_bs"
     fi
   fi
 done < "$links_doc"
 
+if [[ -f "$repo_dir/newlinks" ]]; then
+  source "$repo_dir/newlinks"
+  echo "found new values"
+fi
+
 # prompt new values
-echo "new cdn_css:"
-read cdn_css
-echo "new cdn_jq:"
-read cdn_jq
-echo "new cdn_popper:"
-read cdn_popper
-echo "new cdn_bootstrap:"
-read cdn_bootstrap
+if [ -z "$cdn_css" ];    then echo "new cdn_css:";    read cdn_css; fi;
+if [ -z "$cdn_jq" ];     then echo "new cdn_jq:";     read cdn_jq; fi;
+if [ -z "$cdn_popper" ]; then echo "new cdn_popper:"; read cdn_popper; fi;
+if [ -z "$cdn_bs" ];     then echo "new cdn_bs:";     read cdn_bs; fi;
 
 function process() {
   replace "$old_cdn_css"       "$cdn_css" $1
   replace "$old_cdn_jq"        "$cdn_jq" $1
   replace "$old_cdn_popper"    "$cdn_popper" $1
-  replace "$old_cdn_bootstrap" "$cdn_bootstrap" $1
+  replace "$old_cdn_bs" "$cdn_bs" $1
 }
 
 # file by file in snippets, do replacements
